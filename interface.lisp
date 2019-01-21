343d1bb12144cc10a6c90d6bae31b54ae551bd32
Lee T Ayres <lee.ayres@webcheckout.net>
2019-01-21 12:55:25 -0500

    Option of a graceful failure when the locale is not found in with-locale

--- interface.lisp
+++ interface.lisp
@@ -1,9 +1,9 @@
 (in-package :local-time-locale)
 
-(defmethod %get-locale ((name string))
+(defmethod %get-locale ((name string) &key (on-none :error))
   "We accept either - or _ seperated values in our string and are case
 insenstive"
-  (%get-locale (intern (substitute #\- #\_ (string-upcase name)) 'keyword)))
+  (%get-locale (intern (substitute #\- #\_ (string-upcase name)) 'keyword) :on-none on-none))
 
 (defmethod %shortened-locale ((locale symbol))
   (let ((string (symbol-name locale)))
@@ -11,18 +11,29 @@ insenstive"
       (if p (intern (subseq string 0 p) 'keyword)
 	  nil))))
     
-(defmethod %get-locale ((name symbol))
+(defmethod %get-locale ((name symbol) &key (on-none :error))
   (or (gethash name *locale-cache*)
       (let ((new-locale (make-locale name))
 	    (shortened-locale (%shortened-locale name)))
 	(cond (new-locale
 	       (setf (gethash name *locale-cache*) new-locale))
 	      (shortened-locale
-	       (%get-locale shortened-locale))
-	      (t (error "failed to find locale"))))))
+	       (%get-locale shortened-locale :on-none on-none))
+	      (t (cond
+		   ((eq on-none :error)
+		    (error (format nil "failed to find locale ~S"  name)))
+		   ((eq on-none :warn)
+		    (warn (format nil "failed to find locale ~S"  name)))
+		   ((eq on-none :silent)
+		    ;; no-op
+		    )
+		   (t
+		    (error (format nil "failed to find locale ~S"  name)))))))))
 
-(defmacro with-locale (locale-name &rest body)
-  `(let* ((*default-locale* (%get-locale ,locale-name)))
+;; attempts to set th default local if it can be found.  
+(defmacro with-locale (locale-name &rest body)  
+  `(let* ((*default-locale* (or (%get-locale ,locale-name :on-none :warn)
+				*default-locale*)))
      ,@body))
 
 (defun set-locale (locale-name)
